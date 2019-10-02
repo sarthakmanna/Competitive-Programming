80744c79e80186f01202c936455b1fc8fc6e5d52
Sarthak Manna <34039675+sarthakmanna@users.noreply.github.com>
2019-10-02 12:55:40 +0530

    Update allToOne.py

--- allToOne.py
+++ allToOne.py
@@ -1,36 +1,46 @@
-import os
-inputPath = "/home/sarthak/IdeaProjects/CompetitiveProgramming/src"
-outputPath = "/home/sarthak/Desktop/codes/Main.java"
-
-
-for (parentDir, childDirs, childFiles) in os.walk(inputPath): break
-print ("Merging:", childFiles)
-
-importStatements = set()
-contents = {}
-
-def readFile(fileName):
-	global importStatements, contents
-	file = open(fileName, 'r')
-	lines = file.readlines()
-	truncated = []
-	for l in lines:
-		if 'import' in l: importStatements.add(l)
-		else: truncated.append(l)
-	contents[fileName] = ''.join(truncated)
+while 1:
+	try:
 	
-for file in childFiles: readFile(inputPath + "/" + file)
-
-for mainFile in contents:
-	if all(keyWord in contents[mainFile] for keyWord in ('main', 'args')):
-		print("Main File:", mainFile)
-		break
+		import os
+		inputPath = "/home/sarthak/IdeaProjects/CompetitiveProgramming/src"
+		outputPath = "/home/sarthak/Desktop/codes/Main.java"
 
 
-output = open(outputPath, 'w+')
-output.write(''.join(importStatements) + "\n")
-output.write(''.join(contents[mainFile]) + "\n")
+		for (parentDir, childDirs, childFiles) in os.walk(inputPath): break
+		print ("Merging:", childFiles)
 
-for file in contents:
-	if file != mainFile:
-		output.write(''.join(contents[file]) + "\n")
+		importStatements = set()
+		contents = {}
+
+		def readFile(fileName):
+			global importStatements, contents
+			file = open(fileName, 'r')
+			lines = file.readlines()
+			truncated = []
+			for l in lines:
+				if 'import ' in l: importStatements.add(l)
+				else: truncated.append(l)
+			contents[fileName] = ''.join(truncated)
+			file.close()
+			
+		for file in childFiles: readFile(inputPath + "/" + file)
+
+		for mainFile in contents:
+			if all(keyWord in contents[mainFile] for keyWord in ('main', 'args')):
+				print("Main File:", mainFile)
+				break
+
+
+		output = open(outputPath, 'w+')
+		output.write(''.join(importStatements) + "\n")
+		output.write(''.join(contents[mainFile]) + "\n")
+
+		for file in contents:
+			if file != mainFile:
+				output.write(''.join(contents[file]) + "\n")
+		output.close()
+
+	except: pass
+	
+	import time
+	time.sleep(.5)
