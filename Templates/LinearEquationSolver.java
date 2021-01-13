add7ce8ded5b1f16bb12d8a2764dcb1e28c0bb8c
Sarthak Manna <sarthakmannaofficial@gmail.com>
2021-01-13 21:27:46 +0530

    Added LinearEquationSolver.java

---
+++ Templates/LinearEquationSolver.java
@@ -0,0 +1,40 @@
+package Templates;
+
+import java.util.Arrays;
+
+public class LinearEquationSolver {
+    public double[] solve(double[][] matrix)   // 'matrix' must have dimensions N x (N+1)
+    {
+        double roots[] = new double[matrix.length], toSub;
+        int i, j;
+
+        echelonify(matrix);
+        for(i = matrix.length-1; i >= 0; --i)
+        {
+            if(matrix[i][i] == 0)
+                continue;
+            toSub = 0;
+            for(j = 0; j < matrix[i].length-1; ++j)
+                if(j != i)
+                    toSub += roots[j] * matrix[i][j];
+            roots[i] = (matrix[i][matrix[i].length-1] - toSub) / matrix[i][i];
+        }
+        return roots;
+    }
+
+    public void echelonify(double[][] matrix)
+    {
+        int i, j, k;    double factor;
+        for(i = 0; i < matrix.length; ++i)
+            for(j = 0; j <= i; ++j)
+            {
+                if((factor = matrix[i][j]) == 0)
+                    continue;
+                for(k = 0; k < matrix[i].length; ++k)
+                    matrix[i][k] /= factor;
+                if(i > j)
+                    for(k = 0; k < matrix[i].length; ++k)
+                        matrix[i][k] -= matrix[j][k];
+            }
+    }
+}
