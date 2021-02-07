9df1d90b58dbc3aeafbc2c8b273d5ba12b4abca2
Sarthak Manna <sarthakmannaofficial@gmail.com>
2021-02-07 11:05:03 +0530

    Implemented Comparator in Pair.java (doesn't work for CodeChef as it still uses Java8)

--- Templates/Pair.java
+++ Templates/Pair.java
@@ -1,6 +1,6 @@
 package Templates;
 
-public class Pair<U, V> {
+public class Pair<U, V> implements Comparable<Pair<U, V>> {
     public U getFirst() {
         return first;
     }
@@ -23,6 +23,18 @@ public class Pair<U, V> {
     public Pair(U u, V v) {
         first = u;
         second = v;
+    }
+
+    public int compareTo(Pair<U, V> p) {
+        if (first instanceof Integer) {
+            return Integer.compare((int) first, (int) p.getFirst());
+        } else if (first instanceof Long) {
+            return Long.compare((long) first, (long) p.getFirst());
+        } else if (first instanceof String) {
+            return ((String) first).compareTo((String) p.getFirst());
+        } else {
+            return 5 / 0;
+        }
     }
 
     public String toString() {
