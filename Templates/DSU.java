1ee092ca6c672df699281f186dd7b82dff6cc8d4
Sarthak Manna <sarthakmannaofficial@gmail.com>
2021-03-22 14:40:02 +0530

    Added DSU.java (Disjoint set union template)

---
+++ Templates/DSU.java
@@ -0,0 +1,50 @@
+package Templates;
+
+import java.util.Arrays;
+
+public class DSU {
+    public int getN() {
+        return N;
+    }
+
+    public int[] getParent() {
+        return parent;
+    }
+
+    public int[] getSize() {
+        return size;
+    }
+
+    int N;
+    int[] parent, size;
+
+    public DSU(int nodeCount) {
+        N = nodeCount;
+        parent = new int[nodeCount];
+        Arrays.fill(parent, -7);
+        size = new int[nodeCount];
+        Arrays.fill(size, 1);
+    }
+
+    public int getParent(int node) {
+        if (parent[node] < 0) return node;
+        else return parent[node] = getParent(parent[node]);
+    }
+
+    public boolean isConnected(int a, int b) {
+        return getParent(a) == getParent(b);
+    }
+
+    public void addEdge(int a, int b) {
+        a = getParent(a); b = getParent(b);
+        if (a == b) {
+            return;
+        } else if (size[a] > size[b]) {
+            a ^= b; b ^= a; a ^= b;
+        }
+        parent[a] = b;
+        size[b] += size[a];
+        size[a] = -7;
+    }
+
+}
