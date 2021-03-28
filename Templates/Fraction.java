c8219f375a2504c663a43440ce5ae6478885de9e
Sarthak Manna <sarthakmannaofficial@gmail.com>
2021-03-28 18:28:04 +0530

    Reverted Fraction.java from last commit since it was enhanced already

--- Templates/Fraction.java
+++ Templates/Fraction.java
@@ -71,20 +71,9 @@ public class Fraction {
         return num / (double) den;
     }
 
-    public long modular_pow(long base, long exponent, long MOD) {
-        long result = 1;
-        while (exponent > 0) {
-            if (exponent % 2 == 1)
-                result = (result * base) % MOD;
-            exponent = exponent >> 1;
-            base = (base * base) % MOD;
-        }
-        return result;
-    }
-
     public void power(long exp, long MOD) {
-        num = modular_pow(num, exp, MOD);
-        den = modular_pow(den, exp, MOD);
+        num = hp.pow(num, exp, MOD);
+        den = hp.pow(den, exp, MOD);
     }
 
     public long modValue(long MOD) {
